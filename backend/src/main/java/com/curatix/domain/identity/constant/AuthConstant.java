d3349d2820bb821141e7dfce9af8572b2ddb9950
Ripan Baidya <ripanbaidya7980@gmail.com>
2026-01-22 16:08:48 +0530

    feat (auth):  add endpoints for login and register

---
+++ backend/src/main/java/com/curatix/domain/identity/constant/AuthConstant.java
@@ -0,0 +1,20 @@
+package com.curatix.domain.identity.constant;
+
+/**
+ * Constants related to authentication.
+ */
+public final class AuthConstant {
+
+    private  AuthConstant() {
+    }
+
+    public static  final String ROLE_USER = "ROLE_USER";
+    public static final String ROLE_ADMIN = "ROLE_ADMIN";
+
+    public static final String PASSWORD_POLICY_MESSAGE = "Password must be at least 8-20 characters " +
+            "contain at least one uppercase letter, one lowercase letter, " +
+            "one digit";
+    public static final String PASSWORD_POLICY_EXAMPLE = "SecurePass123";
+
+
+}
